8da8316a86de833d7b108125463c34eb8d9bba63
Elia Kuzmanova <eliamitova7@gmail.com>
2023-02-05 13:57:38 +0100

    add Cube model

--- config/database.js
+++ config/database.js
@@ -3,6 +3,7 @@ const mongoose = require('mongoose');
 const uri = "mongodb://localhost:27017/cubicle"
 
 async function initDatabase() {
+    mongoose.set("strictQuery", false)
 
     await mongoose.connect(uri)
     console.log("DB connected")
---
+++ models/Cube.js
@@ -0,0 +1,35 @@
+const mongoose = require('mongoose')
+
+const cubeSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: true
+    },
+    description: {
+        type: String,
+        required: true,
+        maxLength: 50
+    },
+    imageUrl: {
+        type: String,
+        required: true,
+        validate: {
+            validator: function(value) {
+                return value.startsWith("http://") || value.startsWith("https://")
+            },
+            message: "URL is invalid"
+        }
+
+    },
+    difficultyLevel: {
+        type: Number,
+        required: true,
+        min: 1,
+        max: 6
+    }
+
+})
+
+const Cube = mongoose.model("Cube", cubeSchema)
+
+module.exports = Cube;
